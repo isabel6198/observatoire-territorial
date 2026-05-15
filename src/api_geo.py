3ee06f20b8055ed0ea9a8e1b2820d2f874bba0f6
Isabel Palacio <isabel6198@hotmail.com>
2026-05-15 12:12:35 +0200

    chore(config): mise a jour dependances et gitignore

--- src/api_geo.py
+++ src/api_geo.py
@@ -3,20 +3,15 @@ import requests
 import json
 from pathlib import  Path
 
-
 # Configuration de la requete
 DEPARTEMENT = input("Entrez le code du departement :") 
 BASE_URL = "https://geo.api.gouv.fr"
-
 
 # Chemin de sorti
 output_path = Path("data/raw")
 output_file = output_path / f"communes_{DEPARTEMENT}.json"
 
-
-
 # Construction de la requetes
-
 url = f"{BASE_URL}/departements/{DEPARTEMENT}/communes"
 
 params = {
@@ -25,7 +20,6 @@ params = {
 
 # Appel de l'API pour récupérer les communes du département spécifié
 response = requests.get(url, params=params)
-
 
 # Traitement de la reponse
 if response.status_code == 200:
@@ -42,8 +36,6 @@ if response.status_code == 200:
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
     print(f" Les donnees ont ete enregistrees dans le fichier {output_file}\n")
-
-
     
 else:    print(f"Erreur {response.status_code} lors de la requête" )
 
